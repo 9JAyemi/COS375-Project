5ecccc28f56d4c50bb541fd44c78715b181f4118
Abiola <ab2113@princeton.edu>
2024-10-03 21:22:01 -0400

    updated assem code

--- Project1/Project1--Updated/test/JALtest.asm
+++ Project1/Project1--Updated/test/JALtest.asm
@@ -1,12 +1,15 @@
 # tests jal
+.set noreorder
 Jtest: li   $t5, 30        
       li   $t1, 1
 JALStart: 
-    addi $t5, $t1, 20  
+    addi $t5, $t5, 20  
      jal  JALTestAddr        
     sub  $t5, $t5, $t1
     .word 0xfeedfeed
     
-JALTestAddr: addi $t5, $t1, 2 
+JALTestAddr: addi $t5, $t5, 2 
     jr  $ra #ask abo
+    addi $t3, $t3, 0
+    
 
